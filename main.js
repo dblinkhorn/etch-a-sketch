0aa9dffad000ccab5f2dee31b3956b719efae351
dblinkhorn <dblinkhorn@gmail.com>
2021-04-06 13:41:48 -0700

    layout mostly done. still working on proper screen functionality. need to make hover background color change stay after hover ends.

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-// the script needs to accept an integer user input up to 100
+// the script needs to accept a user input of an integer up to 100
 // using this input, it will create a grid which is comprised of a number of divs on each side equal to the user input
 // once created, the user can hover their mouse over the grid area and any div hovered over will change colors, or have other effects
 // the page will also have a reset button to clear the grid and the user will be able redefine the size of the grid
+
+let screen = document.getElementById("screen");
+
+function addDivs (userNumber) {
+    for (i=0; i < userNumber * userNumber; i++) {
+        let newDiv = document.createElement("div");
+        document.getElementById("screen").appendChild(newDiv);
+        newDiv.id = `div${i}`;
+        newDiv.className = 'newDiv';
+    }
+
+    console.log("addDivs called")
+    console.log(userNumber)
+}
+
+addDivs(50);
