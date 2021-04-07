ce2b747be200d4de841f7ea69d6e6693452bf29e
dblinkhorn <dblinkhorn@gmail.com>
2021-04-06 20:43:23 -0700

    finished logic and design of etch-a-sketch. need to beautify the form and submit buttons at the bottom and link button to javascript to set grid size of screen.

--- main.js
+++ main.js
@@ -4,17 +4,22 @@
 // the page will also have a reset button to clear the grid and the user will be able redefine the size of the grid
 
 let screen = document.getElementById("etch-screen");
+let newDiv;
+let i=0;
+let divArray;
 
 function addDivs (userNumber) {
-    for (i=0; i < userNumber * userNumber; i++) {
-        let newDiv = document.createElement("div");
-        document.getElementById("etch-screen").appendChild(newDiv);
-        newDiv.id = `div${i}`;
-        newDiv.className = 'newDiv';
-    }
 
-    console.log("addDivs called")
-    console.log(userNumber)
+    for (i=0; i < userNumber * userNumber; i++) {
+        newDiv = document.createElement("div");
+        document.getElementById("etch-screen").appendChild(newDiv);
+        newDiv.id = `newDiv${i}`
+        newDiv.className = `newDiv${i}`;
+        width = Math.floor(600/userNumber);
+        newDiv.style.width = `${width}px`;
+        let hoverDiv = document.getElementById(`newDiv${i}`);
+        hoverDiv.addEventListener("mouseover", function(){hoverDiv.style.backgroundColor = "#000"});
+    }
 }
 
-addDivs(50);
+addDivs(100);
